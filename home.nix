39ea170bcb6d4e9a1276516ac915398a081cdf60
Lucas Chaim <lpchaim@gmail.com>
2023-10-11 23:19:20 -0300

    fix: username and home folder

--- home.nix
+++ home.nix
@@ -3,8 +3,8 @@
 {
   # Home Manager needs a bit of information about you and the paths it should
   # manage.
-  home.username = "$USER$";
-  home.homeDirectory = "/home/$USER";
+  home.username = "lpchaim";
+  home.homeDirectory = "/home/lpchaim";
 
   # This value determines the Home Manager release that your configuration is
   # compatible with. This helps avoid breakage when a new Home Manager release
