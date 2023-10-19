076a96dd68efef7492e12d167978b80179e30a03
Lucas Chaim <lpchaim@gmail.com>
2023-10-18 22:21:46 -0300

    refactor: Split editors into separate modules
    Their config is bound to get beefier as times goes on, so may as well

--- editors/module.nix
+++ editors/module.nix
@@ -1,42 +1,12 @@
 { config, pkgs, ... }:
 {
+  imports = [
+    ./helix.nix
+    ./neovim.nix
+  ];
+
   programs = {
-    helix = {
-      enable = true;
-      settings = {
-        theme = "dark_plus";
-        editor = {
-          bufferline = "multiple";
-          color-modes = true;
-          line-number = "relative";
-        };
-        editor.cursor-shape = {
-          insert = "bar";
-          normal = "block";
-          select = "underline";
-        };
-        editor.indent-guides = {
-          render = false;
-          character = "╎";
-          skip-levels = 1;
-        };
-        editor.search = {
-          smart-case = true;
-          wrap-around = true;
-        };
-        editor.statusline = {
-          left = ["mode" "spinner" "file-name"];
-          center = ["version-control"];
-          right = ["diagnostics" "selections" "position" "total-line-numbers" "file-encoding"];
-        };
-        keys.normal = {
-          "A-ç" = "switch_to_uppercase";
-          "ç" = "switch_to_lowercase";
-        };
-      };
-    };
     kakoune.enable = true;
-    neovim.enable = true;
     vim.enable = true;
   };
 }
