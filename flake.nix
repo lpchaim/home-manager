88f75342dc4577e869ce7f34d19e4a87f8efe920
Lucas Chaim <lpchaim@gmail.com>
2023-11-29 12:23:10 -0300

    chore: Add flake.nix

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "Home Manager configuration";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { nixpkgs, flake-utils, home-manager, ... } @inputs:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        makeHomeConfig = { extraModules ? [ ] }:
+          home-manager.lib.homeManagerConfiguration {
+            inherit pkgs;
+            modules = [
+              ./home.nix
+            ] ++ extraModules;
+          };
+      in
+      {
+        packages.homeConfigurations = {
+          cheina = makeHomeConfig { };
+          lpchaim = makeHomeConfig { };
+          lupec = makeHomeConfig { };
+        };
+      }
+    );
+}
